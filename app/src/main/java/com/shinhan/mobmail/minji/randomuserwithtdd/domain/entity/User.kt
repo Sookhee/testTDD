23a62f72a956b6fcc74ee6747321f58347b95a24
minji <minji@mobmail.shinhan.com>
2021-05-03 17:09:14 +0900

    [#1] User 데이터 클래스를 만들고 테스트 케이스 성공시키기

---
+++ app/src/main/java/com/shinhan/mobmail/minji/randomuserwithtdd/domain/entity/User.kt
@@ -0,0 +1,12 @@
+package com.shinhan.mobmail.minji.randomuserwithtdd.domain.entity
+
+data class User(
+    val name: String = "",
+    val age: Int = 0,
+    val gender: String = "",
+    val nation: String = "",
+    val email: String = "",
+    val phone: String = "",
+    val cell: String = "",
+    val profile: String = ""
+)
--- app/src/test/java/com/shinhan/mobmail/minji/randomuserwithtdd/UserTest.kt
+++ app/src/test/java/com/shinhan/mobmail/minji/randomuserwithtdd/UserTest.kt
@@ -1,5 +1,6 @@
 package com.shinhan.mobmail.minji.randomuserwithtdd
 
+import com.shinhan.mobmail.minji.randomuserwithtdd.domain.entity.User
 import junit.framework.Assert.assertEquals
 import org.junit.Test
 
