8c1836e739662fee48a0fafbf6183fa290d8b17d
minji <minji@mobmail.shinhan.com>
2021-05-03 16:55:56 +0900

    [#1] User 데이터 클래스를 만들기 전 반드시 실패하는 테스트 케이스 만들기

---
+++ app/src/test/java/com/shinhan/mobmail/minji/randomuserwithtdd/UserTest.kt
@@ -0,0 +1,20 @@
+package com.shinhan.mobmail.minji.randomuserwithtdd
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class UserTest {
+    @Test
+    fun testUser(){
+        val user = User()
+
+        assertEquals("", user.name)
+        assertEquals(0, user.age)
+        assertEquals("", user.gender)
+        assertEquals("", user.nation)
+        assertEquals("", user.email)
+        assertEquals("", user.phone)
+        assertEquals("", user.cell)
+        assertEquals("", user.profile)
+    }
+}
